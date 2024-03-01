b2ae15174c06a0070756546da3075f571494c09d
Nyendwa <nyendwa@yahoo.com>
2024-03-01 15:56:00 +0200

    first commit

---
+++ login.php
@@ -0,0 +1,50 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Swift</title>
+</head>
+
+<body>
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">SWIFT</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+
+                    <li><a href="#">BUSINESS</a></li>
+                    <li><a href="#">CUSTOMER</a></li>
+                    <li><a href="#">LOG IN</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                </ul>
+
+            </div>
+            <div class="search">
+                <input type="search" class="srch" placeholder="Type to search...">
+                <a href="#"><button class="btn">Search</button></a>
+            </div>
+        </div>
+
+        <div class="content">
+            <div>
+                <h1>Make your money move</h1>
+                <p class="slogan">
+                    Pay online, send money and earn loyalty points with a<br> digital wallet used by millions.
+                </p>
+                <p>
+                    <button><a href="" class="Signup">Sign up now!</a></button>
+                    <button><a href="" class="Learnmore">Learn more</a></button>
+                </p>
+                <button>Register</button>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
